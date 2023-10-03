dc4ce04d29c52a97d3cc3598db25157f9fd201b6
Othi <mnpq.raven@gmail.com>
2023-10-03 17:32:20 +0700

    wip

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -1,8 +1,6 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { DB_AUTH_TOKEN, DB_URL } from '$env/static/private';
 import { createClient } from '@libsql/client/web';
-import { migrate } from 'drizzle-orm/libsql/migrator';
-import { dev } from '$app/environment';
 
 const client = createClient({
   url: DB_URL,
@@ -12,8 +10,3 @@ const client = createClient({
 /** database instance
  * @usage server only */
 export const db = drizzle(client);
-
-if (dev) {
-  // automatically run needed migrations on the database
-  await migrate(db, { migrationsFolder: 'drizzle' });
-}
